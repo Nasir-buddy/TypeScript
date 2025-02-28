849877673d2a9e53b02ac4d53e35f6216792e65a
Nasir-Buddy <syedali172000@gmail.com>
2025-03-01 02:03:43 +0530

    Add Type Inference examples and explanations in TypeInference.js and TypeInference.ts

---
+++ TypeInference/TypeInference.ts
@@ -0,0 +1,15 @@
+// Type Inference
+// Understanding type inference
+// Type annotations
+
+// inference is that the type of the variable is automatically assigned by the compiler
+// we can also define the type of the variable explicitly
+let typeinference = 12;
+
+// but in annoation we have to define the type of the variable explicitly
+let annoation: string = "Nasir";
+let annoation1: number | boolean = true;
+let annoation2: string | number = 12;
+let annoation3: string | number | boolean = true,;
+annoation3 = "Ali";
+annoation3 = 12;
