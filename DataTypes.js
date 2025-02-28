7792dc88a522f5be17f2dbb409606d3e24363ad2
Nasir-Buddy <syedali172000@gmail.com>
2025-03-01 01:41:58 +0530

    Update comments in DataTypes.js for clarity and accuracy

--- DataTypes.js
+++ DataTypes.js
@@ -1,5 +1,5 @@
 "use strict";
-// Basic Types
+// Ignore all comments because it is not valid in JS it is only for TS
 // Premitive Types - number, string, boolean
 // Array 
 // Tuples
@@ -8,9 +8,20 @@
 // Premitive and Reference Types
 // Premitive Types
 // Tuples
+// Tuples are used to store multiple values in a single variable
 let arr = ["Nasir", 1];
 let arr1 = [17, "Ali"];
 // Enumerations
+// Enums allow us to define a set of named constants
+// Either numeric or string values
+// Enums values are case-sensitive
+// Enums may have initializers
+// Enums are used to define a set of named constants
+// Enums are defined using the enum keyword
+// Enums are strongly typed and are not the same as objects
+// Enums values are accessed using the dot notation
+// Enums can be looped over
+// Enums can be merged
 var UserRoles;
 (function (UserRoles) {
     // In Enumeratoin we can use create properties with values 
@@ -25,4 +36,11 @@ var Statuscode;
     Statuscode[Statuscode["UNAUTHORIZED"] = 401] = "UNAUTHORIZED";
 })(Statuscode || (Statuscode = {}));
 // Any, Unknown, Void, Null, Undefined, Never
-let a;
+// Any
+// Any is a data type that we can use when we are not sure about the type of the value
+// Any is a dynamic type   
+// Any type can store any type of value 
+let a; // By default it is any type
+// that variable can be assigned any type of value
+a = 10;
+a = "Nasir";
