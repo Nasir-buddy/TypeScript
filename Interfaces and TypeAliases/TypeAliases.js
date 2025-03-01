e73b37da0a637cc2ad5d42fd26d8171099d41ae9
Nasir-Buddy <syedali172000@gmail.com>
2025-03-01 16:25:12 +0530

    Add Type Aliases examples in TypeAliases.js and TypeAliases.ts

---
+++ Interfaces and TypeAliases/TypeAliases.js
@@ -0,0 +1,7 @@
+"use strict";
+// Type Aliases
+let t;
+let t1;
+// assigning value to of value we can assign string, number or null in the function 
+function ex(obj) {
+}
---
+++ Interfaces and TypeAliases/TypeAliases.ts
@@ -0,0 +1,13 @@
+// Type Aliases
+
+type sankhya = number;
+let t: sankhya;
+
+// real life ex
+type value = string | number | null
+let t1: value;
+
+// assigning value to of value we can assign string, number or null in the function 
+function ex(obj: value){
+    
+}
