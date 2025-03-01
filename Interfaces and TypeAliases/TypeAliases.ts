5a7904560fcfd3b1ad5aeeee6cd39b2a121327d1
Nasir-Buddy <syedali172000@gmail.com>
2025-03-01 16:26:21 +0530

    Update file permissions for DataTypes.js, TypeAliases.js, TypeAliases.ts, and TypeInference.js; enhance Type Aliases examples in TypeAliases.ts

--- Interfaces and TypeAliases/TypeAliases.ts
+++ Interfaces and TypeAliases/TypeAliases.ts
@@ -1,4 +1,5 @@
 // Type Aliases
+// typs is used to create a new name for a type
 
 type sankhya = number;
 let t: sankhya;
@@ -10,4 +11,20 @@ let t1: value;
 // assigning value to of value we can assign string, number or null in the function 
 function ex(obj: value){
     
+}
+
+
+type users = {
+    name: string,
+    age: number
+}
+
+type user2 = users & {
+    getNmae(user: string): void
+}
+
+function getName(a: user2){
+    a.getNmae('sankhya');
+    a.age;
+    a.name;
 }
