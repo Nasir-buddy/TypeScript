2d19135743fa8d926ca9ff97e1659e987f4d1014
Nasir-Buddy <syedali172000@gmail.com>
2025-03-01 16:51:25 +0530

    Add BottleMaker class implementation and examples in classes&objects.js and classes&objects.ts

--- Classes and Objects/classes&objects.ts
+++ Classes and Objects/classes&objects.ts
@@ -7,3 +7,16 @@ class Device{
 let device = new Device();;
 let device1 = new Device();
 console.log(device.name);
+
+// constructer is that thing that will give the structure of the real data but not the data itself
+// like a machine making biscuits, the machine is the constructer and the biscuits are the objects
+// the machine will make biscuits but it will not be the biscuit itself
+// the machine will have the structure of the biscuit but not the biscuit itself
+
+class BottleMaker{
+    constructor(public name: string, public price: number){
+        
+    }
+}
+
+let bottle = new BottleMaker("Milton", 1200); // this is the object of the class BottleMaker
