cf9f3a549b3e3db3c706f1862ca62b86069aa6f4
Nasir-Buddy <syedali172000@gmail.com>
2025-03-02 23:59:22 +0530

    Add AccessModifiers class implementation in AccessModifiers.js and AccessModifiers.ts

---
+++ Classes and Objects/AccessModifiers/AccessModifiers.js
@@ -0,0 +1,13 @@
+"use strict";
+class BottleMaker1 {
+    // public means we change change it from any where like class methods or outside the class.
+    // private means we cannot change it from outside the class
+    // protected means we can change it from the class and its child class but not from outside the class
+    // readonly means we can only read it but cannot change it
+    // static means we can call it without making an object of the class
+    constructor(name) {
+        this.name = name;
+    }
+}
+let bt1 = new BottleMaker1("Milton");
+bt1.name = "CocaCola";
