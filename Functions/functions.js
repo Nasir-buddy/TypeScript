5512a6f48349a3611bef21d706719ace52f2ff1b
Nasir-Buddy <syedali172000@gmail.com>
2025-03-05 02:21:02 +0530

    add function and fat arrow function

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,9 +1,18 @@
-"use strict";
 // Functions
 // function types
 // optional and default function parameter
 // rest parameter
 // overloads
-function login() {
+function login(name, cb) {
     // your code here for login
+    cb("Nasir");
 }
+login("harsh", function (value) {
+    console.log(value);
+});
+var register = function (name, age, callback) {
+    callback("Hello my name is Nasir Ali");
+};
+register("Nasir Ali", 24, function (argument) {
+    console.log(argument);
+});
--- Functions/functions.ts
+++ Functions/functions.ts
@@ -4,7 +4,19 @@
 // rest parameter
 // overloads
 
-function login(){
+function login(name: string, cb: (value: string) => void){
     // your code here for login
+    cb("Nasir")
 }
 
+login("harsh", (value: string) => {
+    console.log(value);  
+});
+
+const register = (name: string, age: number, callback: (value: string) => void):void => {
+    callback("Hello my name is Nasir Ali");
+}
+
+register("Nasir Ali", 24, (argument: string) => {
+    console.log(argument);
+})
