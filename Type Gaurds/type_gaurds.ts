a04a718a881212b76f7d0048ebe754dea465c246
Nasir-Buddy <syedali172000@gmail.com>
2025-03-08 02:51:10 +0530

    type gaurd

--- Type Gaurds/type_gaurds.ts
+++ Type Gaurds/type_gaurds.ts
@@ -2,3 +2,22 @@
 // using typeof and instanceof
 // partial, required, readonly
 
+// type gaurd is also like type narrowing
+function something(arg: string | number | any){
+    if(typeof arg == "number"){
+        // all auto functon for number
+        // arg.(<all functoins>) 
+        return "number"
+    } else if(typeof arg == "string"){
+        // all auto function for string
+        // arg.(<all functoins>)
+        return "string"
+    } else {
+        throw new Error("nothing matched...")
+    }
+}
+console.log(something(17));
+console.log(something("Nasir Ali"));
+
+something(true);
+
