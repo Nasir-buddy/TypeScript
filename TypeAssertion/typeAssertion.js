2335bb2b89d550476950f39edbb6a80bc7a53642
Nasir-Buddy <syedali172000@gmail.com>
2025-03-08 02:37:09 +0530

    type assertion and type casting

---
+++ TypeAssertion/typeAssertion.js
@@ -0,0 +1,6 @@
+"use strict";
+// first of all type casting and type assertion is most likely same thing
+// type assertion ka matlab btana ts ko ki particular cheej ka type kya hai , 
+// ye ap tab krte ho jb aap ts se jyada us value aur variable ke bare me jante ho.
+// ex
+let d = 12;
---
+++ TypeAssertion/typeAssertion.ts
@@ -0,0 +1,7 @@
+// first of all type casting and type assertion is most likely same thing
+// type assertion ka matlab btana ts ko ki particular cheej ka type kya hai , 
+// ye ap tab krte ho jb aap ts se jyada us value aur variable ke bare me jante ho.
+
+// ex
+
+let d: any = 12;
